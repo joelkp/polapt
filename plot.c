148fbe50fd69d7744f93c830563769d14120baa4
Joel K. Pettersson <joelkpettersson@gmail.com>
2021-08-02 16:43:46 +0200

    refactor

--- plot.c
+++ plot.c
@@ -18,18 +18,21 @@
 #include <stdio.h>
 #include <math.h>
 
-static inline float SGS_sinf_t7(float x) {
+/* max error: 1.388788e-05 */
+static inline float SGS_sinf_t7_old(float x) {
 	const float scale7 = -1.f/5040 * 29.f/30;
 	float x2 = x*x;
 	return x + x*x2*(-1.f/6 + x2*(1.f/120 + x2*scale7));
 }
 
-static inline float SGS_sinf_t9(float x) {
+/* max error: 3.576279e-07 */
+static inline float SGS_sinf_t9_old(float x) {
 	const float scale9 = 1.f/362880 * 44.f/45;
 	float x2 = x*x;
 	return x + x*x2*(-1.f/6 + x2*(1.f/120 + x2*(-1.f/5040 + x2*scale9)));
 }
 
+/* max error: 1.788139e-07 */
 static inline float moo_sine(float x) {
 	const float coeffs[] = {
 		-0.10132118f,          // x
@@ -51,13 +54,6 @@ static inline float moo_sine(float x) {
 	return (x - pi_major - pi_minor) * (x + pi_major + pi_minor) * p1 * x;
 }
 
-/* two varied */
-//	const float scale5 = 1.f/120 * (869.f/870);
-//	const float scale7 = -1.f/5040 * 18.f/19;
-/* three varied */
-//	const float scale3 = -1.f/6 * (15000.f/15001);
-//	const float scale5 = 1.f/120 * (359.f/360);
-//	const float scale7 = -1.f/5040 * 13.f/14;
 static inline float test_sin(float x, double scale_adj[]) {
 	const float scale[] = {
 		-1.f/6 * scale_adj[0],
@@ -74,99 +70,82 @@ float selerr_sinf[LENGTH];
 float curerr_sinf[LENGTH];
 float maxerr_sinf = 1.f; // large-enough start value to accept any contender
 
-#define A_TRY 100000 //10000 //100000
+#define PDIM 3
+double scale_adj[PDIM] = {1.f, 1.f, 1.f};
+int scale_report[PDIM][2];
+
+#define A_TRY 100000 //100000
 #define B_TRY 1000  //10000
 #define C_TRY 100   //1000
 
+static int test_candidate(int pcoeffs[PDIM]) {
+	double try_scale_adj[PDIM];
+	for (int j = 0; j < PDIM; ++j) {
+		int q = pcoeffs[j];
+		if (q == 0) {
+			try_scale_adj[j] = 1.f;
+		} else {
+			try_scale_adj[j] = ((double)(q - 1) / (double) q);
+		}
+	}
+	for (int i = 0, end = LENGTH; i < end; ++i) {
+		float x = (i * 1.f/(end - 1) - 0.5f);
+		float err = good_sinf[i] -
+		test_sin(x * M_PI, try_scale_adj);
+		if (fabs(err) > maxerr_sinf)
+			return 0;
+		curerr_sinf[i] = err;
+	}
+	/*
+	 * New selection made...
+	 */
+	float selmaxerr = 0.f;
+	for (int i = 0, end = LENGTH; i < end; ++i) {
+		if (selmaxerr < curerr_sinf[i])
+			selmaxerr = curerr_sinf[i];
+		selerr_sinf[i] = curerr_sinf[i];
+	}
+	maxerr_sinf = selmaxerr;
+	for (int j = 0; j < PDIM; ++j) {
+		int q = pcoeffs[j];
+		if (q == 0) {
+			scale_report[j][0] = 0;
+			scale_report[j][1] = 0;
+		} else {
+			scale_report[j][0] = q - 1;
+			scale_report[j][1] = q;
+		}
+		scale_adj[j] = try_scale_adj[j];
+	}
+	return 1;
+}
+
 int main() {
 	FILE *f = fopen("plot.txt", "w");
-	double scale_adj[3] = {1.f, 1.f, 29.f/30};
-	int scale_report[3][2];
-	double try_scale_adj[3];
-	int new_report = 0, want_new_report = 1;
+	int print_report = 1, new_report = 0;
 	for (int i = 0, end = LENGTH; i < end; ++i) {
 		float x = (i * 1.f/(end - 1) - 0.5f);
 		good_sinf[i] = sinf(x * M_PI);
 	}
 	for (int A = 0; A <= A_TRY + 1; ++A) {
-		if (A == 0) {
-			try_scale_adj[0] = 1.f;
-		} else {
-			try_scale_adj[0] = ((double)(A - 1) / (double) A);
-		}
 		for (int B = 0; B <= B_TRY + 1; ++B) {
-			if (B == 0) {
-				try_scale_adj[1] = 1.f;
-			} else {
-				try_scale_adj[1] =
-					((double)(B - 1) / (double) B);
-			}
 			for (int C = 0; C <= C_TRY + 1; ++C) {
-				if (C == 0) {
-					try_scale_adj[2] = 1.f;
-				} else {
-					if (C == C_TRY && (!B || B == B_TRY))
-						want_new_report = 1;
-					try_scale_adj[2] =
-						((double)(C - 1) / (double) C);
-				}
-				for (int i = 0, end = LENGTH; i < end; ++i) {
-					float x = (i * 1.f/(end - 1) - 0.5f);
-					float err = good_sinf[i] -
-						test_sin(x * M_PI,
-								try_scale_adj);
-					if (fabs(err) > maxerr_sinf)
-						goto SKIP;
-					curerr_sinf[i] = err;
-				}
-				/*
-				 * New selection made...
-				 */
-				float selmaxerr = 0.f;
-				for (int i = 0, end = LENGTH; i < end; ++i) {
-					if (selmaxerr < curerr_sinf[i])
-						selmaxerr = curerr_sinf[i];
-					selerr_sinf[i] = curerr_sinf[i];
-				}
-				maxerr_sinf = selmaxerr;
-				new_report = 1;
-				scale_adj[0] = try_scale_adj[0];
-				scale_adj[1] = try_scale_adj[1];
-				scale_adj[2] = try_scale_adj[2];
-				if (A == 0) {
-					scale_report[0][0] = 0;
-					scale_report[0][1] = 0;
-				} else {
-					scale_report[0][0] = A - 1;
-					scale_report[0][1] = A;
-				}
-				if (B == 0) {
-					scale_report[1][0] = 0;
-					scale_report[1][1] = 0;
-				} else {
-					scale_report[1][0] = B - 1;
-					scale_report[1][1] = B;
-				}
-				if (C == 0) {
-					scale_report[2][0] = 0;
-					scale_report[2][1] = 0;
-				} else {
-					scale_report[2][0] = C - 1;
-					scale_report[2][1] = C;
-				}
-			SKIP:
-				if (new_report && want_new_report) {
+				if (C == C_TRY && (!B || B == B_TRY))
+					print_report = 1;
+				int pcoeffs[PDIM] = {A, B, C};
+				if (test_candidate(pcoeffs))
+				       new_report = 1;
+				if (new_report && print_report) {
 					new_report = 0;
-					want_new_report = 0;
+					print_report = 0;
 					printf(
-"Max.err. %.11f\nA==%.11f\t(%d, %d)\nB==%.11f\t(%d, %d)\nC==%.11f\t(%d, %d)\n",
+"Max.err. %e\nA==%.11f\t(%d, %d)\nB==%.11f\t(%d, %d)\nC==%.11f\t(%d, %d)\n",
 maxerr_sinf,
 scale_adj[0], scale_report[0][0], scale_report[0][1],
 scale_adj[1], scale_report[1][0], scale_report[1][1],
 scale_adj[2], scale_report[2][0], scale_report[2][1]
 						);
 				}
-				continue;
 			}
 		}
 	}
@@ -175,10 +154,5 @@ scale_adj[2], scale_report[2][0], scale_report[2][1]
 		fprintf(f, "%.11f\t%.11f\n", x, selerr_sinf[i]);
 	}
 	fclose(f);
-//	printf("A==%.11f\t(%d, %d)\nB==%.11f\t(%d, %d)\nC==%.11f\t(%d, %d)\n",
-//			scale_adj[0], scale_report[0][0], scale_report[0][1],
-//			scale_adj[1], scale_report[1][0], scale_report[1][1],
-//			scale_adj[2], scale_report[2][0], scale_report[2][1]
-//			);
 	return 0;
 }
